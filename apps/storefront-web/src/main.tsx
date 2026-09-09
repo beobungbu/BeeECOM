da0e2a1fd76fa5b649046b0f04200037e8372390
Lan Tran Duc <lantranduc@gmail.com>
2026-09-09 17:19:27 +0700

    feat(theme): mount storefront theme root

--- apps/storefront-web/src/main.tsx
+++ apps/storefront-web/src/main.tsx
@@ -2,13 +2,13 @@ import './global.css';
 
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './App';
+import { ThemeRoot } from './ThemeRoot';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Root container #root was not found.');
 
 createRoot(container).render(
   <React.StrictMode>
-    <App />
+    <ThemeRoot />
   </React.StrictMode>,
 );
