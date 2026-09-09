d11953d203eb871bde8e57c37a3f50e876f9f8db
Lan Tran Duc <lantranduc@gmail.com>
2026-09-09 12:46:20 +0700

    chore(mobile): remove duplicate Metro config

--- apps/mobile/metro.config.cjs
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withUniwindConfig } = require('uniwind/metro');
-
-module.exports = withUniwindConfig(getDefaultConfig(__dirname), {
-  cssEntryFile: './global.css',
-  dtsFile: './uniwind-types.d.ts',
-  extraThemes: ['violet-light', 'violet-dark', 'high-contrast-light', 'high-contrast-dark'],
-});
